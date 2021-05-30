e8af225028abccb08a2539a03b88fcb6caa27cd4
tushal012 <tushalkashyap11@gmail.com>
2021-05-30 21:22:19 +0530

    ADDED: first name is added

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,4 +1,14 @@
 package com.bridgelabz;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    private static final String pattern_name="^[A-z]{1}[a-z]{2,}$";
+    private static final String pattern_Email="^[a-z]{3}\\.[a-zA-Z]{3}\\@[a-zA-Z]{3,10}\\.[co]{2}\\.[a-z]{2}$";
+    private static final String pattern_Phone="^[0-9]{2}\\s[0-9]{10}$";
+    private static final String pattern_Password="^(?=.*?[A-Z])(?=.*?[0-9])(?=.*[@$!%*#?&]).{8,}";
+    public boolean toValidateFirstName(String firstName) {
+        boolean matches= Pattern.matches(pattern_name,firstName);
+        return matches;
+    }
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,2 +1,15 @@
+import com.bridgelabz.UserRegistration;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+    @Test
+    public void ValidateFirstName()
+    {
+        UserRegistration registration=new UserRegistration();
+        assertEquals(true,registration.toValidateFirstName("Tushal"));
+    }
+
+    private void assertEquals(boolean b, boolean tushal) {
+    }
+
 }
